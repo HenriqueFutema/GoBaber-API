5b775ca3eff29b50786d414560dae67f03758197
HenriqueFutema <minifut0407@gmail.com>
2019-10-09 17:31:37 -0300

    Listagem dos proividers

--- src/app.js
+++ src/app.js
@@ -1,29 +1,28 @@
-import express from 'express'
-import routes from './routes'
+import express from "express";
+import path from "path";
+import routes from "./routes";
 
-import './database'
+import "./database";
 
 class App {
     constructor() {
-        this.server = express()
+        this.server = express();
 
-        this.middlewares()
-        this.routes()
-
+        this.middlewares();
+        this.routes();
     }
 
     middlewares() {
-
-        this.server.use(express.json())
-
+        this.server.use(express.json());
+        this.server.use(
+            "/files",
+            express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
+        );
     }
 
     routes() {
-
-        this.server.use(routes)
-
+        this.server.use(routes);
     }
-
 }
 
-module.exports = new App().server
+module.exports = new App().server;
