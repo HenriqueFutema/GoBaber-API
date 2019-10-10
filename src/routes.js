4228c17c2f63476c69c467fd627cb0d4a1c575d1
HenriqueFutema <minifut0407@gmail.com>
2019-10-10 20:59:55 -0300

    NotificationsController -> index

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import SessionController from "./app/controllers/SessionController";
 import FileController from "./app/controllers/FileController";
 import AppointmentController from "./app/controllers/AppointmentController";
 import ScheduleController from "./app/controllers/ScheduleController";
+import NotificationController from './app/controllers/NotificationController'
 
 
 import authMiddleware from "./app/middlewares/auth";
@@ -28,7 +29,9 @@ routes.get("/providers", ProviderController.index);
 routes.get("/appointments", AppointmentController.index);
 routes.post("/appointments", AppointmentController.store);
 
-routes.get('/schedule', ScheduleController.index)
+routes.get("/schedule", ScheduleController.index)
+
+routes.get("/notifications", NotificationController.index)
 
 routes.post("/files", upload.single("file"), FileController.store);
 
